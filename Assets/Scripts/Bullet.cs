d4ddddefc9c98943e743ec38e6e43b509f713fa8
Jacob Cheatley <jacobcheatley@gmail.com>
2017-12-05 15:51:52 +1300

    Woops, I added a bunch of stuff

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+public class Bullet : MonoBehaviour
+{
+    public PlayerIndex playerIndex;
+
+    private Collider col;
+
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject g = other.gameObject;
+
+//        if (g.tag == "Player" && g.GetComponent<Player>().playerIndex == playerIndex)
+//        {
+//            Physics.IgnoreCollision(other.col, col);
+//            Debug.Log("Ignored");
+//        }
+
+        if (g.tag == "Bullet" && g.GetComponent<Bullet>().playerIndex == playerIndex)
+        {
+            Physics.IgnoreCollision(other.collider, col);
+            Debug.Log("Ignored");
+        }
+
+        Debug.Log(g.tag);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,18 @@ public class Player : MonoBehaviour
 {
     // Editor Fields
     [Header("References")]
+    [SerializeField] private Transform bulletOrigin;
+    [SerializeField] private GameObject bulletPrefab;
     [Header("Control")]
-    [SerializeField] private PlayerIndex playerIndex;
+    [SerializeField] public PlayerIndex playerIndex;
     [SerializeField] private float maxSpeed = 8.0f;
     [Header("Dash")]
     [SerializeField] private float dashCooldown = 2.0f;
     [SerializeField] private float dashSpeed = 30.0f;
     [SerializeField] private float dashDuration = 0.2f;
+    [Header("Shooting")]
+    [SerializeField] private float shootCooldown = 0.2f;
+    [SerializeField] private float bulletSpeed = 40f;
 
     // Constants
     private const float deadMag = 0.1f;
@@ -20,9 +25,10 @@ public class Player : MonoBehaviour
 
     // Private Variables
     private Rigidbody rb;
-    private Vector3 currentVelocity = Vector3.zero;
+    private Collider col;
     private bool dashing = false;
     private bool canDash = true;
+    private bool canShoot = true;
     private GamePadState state;
     private GamePadState prevState;
 
@@ -34,6 +40,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = rb.GetComponent<Collider>();
     }
 
     void Update()
@@ -41,36 +48,72 @@ public class Player : MonoBehaviour
         prevState = state;
         state = GamePad.GetState(playerIndex);
 
-        #region Movement
+        Movement();
+        Shooting();
+        Effects();
+    }
+
+    private void Movement()
+    {
         Vector3 leftStick = Vector3.ClampMagnitude(new Vector3(state.ThumbSticks.Left.X, 0, state.ThumbSticks.Left.Y), 1);
         bool leftTriggerPress = state.Triggers.Left >= triggerThreshold;
 
         if (canDash && leftTriggerPress && leftStick.sqrMagnitude >= 0.5f)
         {
-            dashing = true;
-            canDash = false;
             StartCoroutine(Dash(leftStick.normalized));
         }
 
         if (!dashing)
             rb.velocity = leftStick * maxSpeed;
+    }
 
-        #endregion
-
-        #region Turning
-        Vector3 rightStick = Vector3.ClampMagnitude(new Vector3(state.ThumbSticks.Right.X, 0, state.ThumbSticks.Right.Y), 1);
+    private void Shooting()
+    {
+        Vector3 rightStick = new Vector3(state.ThumbSticks.Right.X, 0, state.ThumbSticks.Right.Y).normalized;
         if (rightStick.sqrMagnitude >= deadMag)
+        {
             gameObject.transform.rotation = Quaternion.LookRotation(rightStick);
-        #endregion
+            Shoot(rightStick);
+        }
+    }
+
+    private void Effects()
+    {
+        float left = dashing ? 0.5f : 0;
+        float right = (canShoot ? 0 : 0.2f) + (dashing ? 0.1f : 0);
+        GamePad.SetVibration(playerIndex, left, right);
+    }
+
+    private void Shoot(Vector3 direction)
+    {
+        if (canShoot)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, bulletOrigin.position, Quaternion.LookRotation(direction));
+            bullet.GetComponent<Rigidbody>().velocity = direction * bulletSpeed;
+            bullet.GetComponent<Bullet>().playerIndex = playerIndex;
+            Physics.IgnoreCollision(col, bullet.GetComponent<Collider>());
+            StartCoroutine(Reload());
+            Destroy(bullet, 1f);
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootCooldown);
+        canShoot = true;
     }
 
     private IEnumerator Dash(Vector3 direction)
     {
+        dashing = true;
+        canDash = false;
         rb.velocity = dashSpeed * direction;
+        GamePad.SetVibration(playerIndex, 0.3f, 0.1f);
         yield return new WaitForSeconds(dashDuration);
+        GamePad.SetVibration(playerIndex, 0, 0);
         dashing = false;
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
-        Debug.Log("Dash Cooled Down");
     }
 }
