1946d5a8b4f69013c435c897ad93c867ce023ba0
Jacob Cheatley <jacobcheatley@gmail.com>
2017-12-08 11:52:45 +1300

    Basic player slot joining lobby

---
+++ Assets/Scripts/UI/PlayerJoin.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+public class PlayerJoin : MonoBehaviour
+{
+    [SerializeField] private GameObject slotPrefab;
+
+    private PlayerSlot[] slots = new PlayerSlot[4];
+    private bool[] joined = { false, false, false, false };
+    private int currentSlot;
+
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject slot = Instantiate(slotPrefab, transform);
+            RectTransform rt = slot.GetComponent<RectTransform>();
+            rt.anchorMin = rt.anchorMax = new Vector2(i / 3f, 0.5f);
+            slot.SetActive(false);
+            slots[i] = slot.GetComponent<PlayerSlot>();
+        }
+
+        currentSlot = 0;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            PlayerIndex index = (PlayerIndex) i;
+            GamePadState state = GamePad.GetState(index);
+
+            if (state.Buttons.A == ButtonState.Pressed && !joined[i])
+            {
+                joined[i] = true;
+                AssignNextSlot(index);
+            }
+        }
+    }
+
+    void AssignNextSlot(PlayerIndex index)
+    {
+        slots[currentSlot].Init(index);
+        currentSlot++;
+    }
+}
---
+++ Assets/Scripts/UI/PlayerSlot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class PlayerSlot : MonoBehaviour
+{
+    [SerializeField] private GameObject playerIndicator;
+
+    private PlayerIndex index;
+
+    public void Init(PlayerIndex index)
+    {
+        this.index = index;
+
+        switch (index)
+        {
+            case PlayerIndex.One:
+                playerIndicator.transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case PlayerIndex.Two:
+                playerIndicator.transform.rotation = Quaternion.Euler(0, 0, 270);
+                break;
+            case PlayerIndex.Three:
+                playerIndicator.transform.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case PlayerIndex.Four:
+                playerIndicator.transform.rotation = Quaternion.Euler(0, 0, 180);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(index), index, null);
+        }
+
+        gameObject.SetActive(true);
+    }
+
+    // TODO: Cycle through colour, start game (?) when everyone's ready
+    // Maps flow - WTF do?
+}
