d080974af27bcbf01b866e076f01885f4ca0dbad
Jacob Cheatley <jacobcheatley@gmail.com>
2018-01-15 14:59:24 +1300

    Timer ticks down

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -4,6 +4,7 @@ public class InGameUI : MonoBehaviour
 {
     [SerializeField] private RectTransform statCardContainer;
     [SerializeField] private GameObject statCardPrefab;
+    [SerializeField] private TimerUI timerUi;
 
     public void Init()
     {
@@ -13,5 +14,6 @@ public class InGameUI : MonoBehaviour
             Instantiate(statCardPrefab, statCardContainer).GetComponent<StatCard>().Init(slotInfo, count);
             count++;
         }
+        timerUi.Init();
     }
 }
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField] private int roundTime = 90;
+    [SerializeField] private Text text;
+
+    private int timeRemaining;
+
+    public void Init()
+    {
+        timeRemaining = roundTime;
+        StartCoroutine(CountDown());
+    }
+
+    private IEnumerator CountDown()
+    {
+        while (timeRemaining > 0)
+        {
+            text.text = timeRemaining.ToString();
+            yield return new WaitForSeconds(1);
+            timeRemaining--;
+        }
+    }
+}
